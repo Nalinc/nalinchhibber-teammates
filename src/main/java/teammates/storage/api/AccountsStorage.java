7c326213a7a3ef9b4ab9a661eb6a2028784f74ee
Damith Rajapakse <damith@gmail.com>
2012-07-09 17:55:27 +0800

    [mq]: uiComponentDiagram

---
+++ src/main/java/teammates/storage/api/AccountsStorage.java
@@ -0,0 +1,268 @@
+package teammates.storage.api;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+
+import teammates.common.Common;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.EntityDoesNotExistException;
+import teammates.storage.datastore.Datastore;
+import teammates.storage.entity.Coordinator;
+import teammates.storage.entity.Student;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+/**
+ * Accounts handles all operations related to a Teammates account.
+ * @see Coordinator
+ * @see Student
+ * 
+ */
+public class AccountsStorage {
+
+	private static UserService userService;
+	private static AccountsStorage instance = null;
+	private static final Logger log = Common.getLogger();
+
+	/**
+	 * Constructs an Accounts object. Initialises userService for Google User
+	 * Service and obtains an instance of PersistenceManager class to handle
+	 * datastore transactions.
+	 */
+	private AccountsStorage() {
+		userService = UserServiceFactory.getUserService();
+	}
+	private PersistenceManager getPM() {
+		return Datastore.getPersistenceManager();
+	}
+
+	public static AccountsStorage inst() {
+		if (instance == null)
+			instance = new AccountsStorage();
+		return instance;
+	}
+
+	/**
+	 * See if the current user is authenticated
+	 * 
+	 * @return
+	 */
+	public boolean isLoggedOn() {
+		return userService.getCurrentUser() != null;
+	}
+
+	/**
+	 * Adds a Coordinator object.
+	 * 
+	 * @param googleID
+	 *            the coordinator's Google ID (Precondition: Must not be null)
+	 * 
+	 * @param name
+	 *            the coordinator's name (Precondition: Must not be null)
+	 * 
+	 * @param email
+	 *            the coordinator's email (Precondition: Must not be null)
+	 * 
+	 * 
+	 */
+	public void addCoordinator(String googleID, String name, String email)
+			throws EntityAlreadyExistsException {
+		if (getCoordinator(googleID) != null) {
+			throw new EntityAlreadyExistsException("Coordinator already exists :" + googleID);
+		}
+		Coordinator coordinator = new Coordinator(googleID, name, email);
+		getPM().makePersistent(coordinator);
+	}
+
+	/**
+	 * Returns a Coordinator object.
+	 * 
+	 * @param googleID
+	 *            the coordinator's Google ID (Precondition: Must not be null)
+	 * 
+	 * @return the coordinator with the specified Google ID, or null if not
+	 *         found
+	 */
+	public Coordinator getCoordinator(String googleID) {
+		String query = "select from " + Coordinator.class.getName()
+				+ " where googleID == '" + googleID + "'";
+
+		@SuppressWarnings("unchecked")
+		List<Coordinator> coordinatorList = (List<Coordinator>) getPM()
+				.newQuery(query).execute();
+
+		if (coordinatorList.isEmpty()) {
+			log.warning("Trying to get non-existent Coord : " + googleID);
+			return null;
+		}
+
+		return coordinatorList.get(0);
+	}
+	
+	public Student getStudent(String courseId, String email) {
+		String query = "select from " + Student.class.getName()
+				+ " where (email == '" + email + "')"
+				+ " && (courseID == '" + courseId + "')";
+
+		@SuppressWarnings("unchecked")
+		List<Student> studentList = (List<Student>) getPM()
+				.newQuery(query).execute();
+
+		if (studentList.isEmpty()) {
+			log.fine("Trying to get non-existent Student : " + courseId
+					+ "/" + email);
+			return null;
+		}
+		Student student = studentList.get(0);
+		return student;
+	}
+	
+	public List<Student> getStudentsWithID(String googleId) {
+		String query = "select from " + Student.class.getName() + " where ID == \"" + googleId + "\"";
+
+		@SuppressWarnings("unchecked")
+		List<Student> studentList = (List<Student>) getPM().newQuery(query).execute();
+
+		return studentList;
+	}
+	
+	public boolean isStudent(String googleId){
+		return getStudentsWithID(googleId).size()!=0;
+	}
+
+	/**
+	 * Returns the list of Coordinator objects.
+	 * 
+	 * @return the list of coordinators
+	 */
+	private List<Coordinator> getCoordinatorList() {
+		String query = "select from " + Coordinator.class.getName();
+
+		@SuppressWarnings("unchecked")
+		List<Coordinator> coordinatorList = (List<Coordinator>) getPM()
+				.newQuery(query).execute();
+
+		return coordinatorList;
+	}
+
+	/**
+	 * Returns the name of the Coordinator object given his googleID.
+	 * 
+	 * @param googleID
+	 *            the coordinator's Google ID (Precondition: Must not be null)
+	 * 
+	 * @return the name of the coordinator
+	 */
+	public String getCoordinatorName(String googleID) {
+		Coordinator coordinator = getCoordinator(googleID);
+
+		return coordinator.getName();
+	}
+
+	/**
+	 * Returns the login page to the user, or the designated redirect page if
+	 * the user is already logged in.
+	 * 
+	 * @param redirectPage
+	 *            the page to redirect the user (Precondition: Must not be null)
+	 * 
+	 * @return the login page or redirect page if the user is already logged in
+	 */
+	public String getLoginPage(String redirectPage) {
+		// Check if user is already logged in
+		User user = userService.getCurrentUser();
+
+		if (user != null) {
+			// Direct user to his chosen page
+			return redirectPage;
+		}
+
+		else {
+			// Direct user to Google Login first before chosen page
+			return userService.createLoginURL(redirectPage);
+		}
+	}
+
+	/**
+	 * Returns the logout page to the user.
+	 * 
+	 * @param redirectPage
+	 *            the page to redirect the user (Precondition: Must not be null)
+	 * 
+	 * @return the redirect page after logout
+	 */
+	public String getLogoutPage(String redirectPage) {
+		return userService.createLogoutURL(redirectPage);
+	}
+
+	/**
+	 * Returns the Google User object.
+	 * 
+	 * @return the user
+	 */
+	public User getUser() {		
+		return userService.getCurrentUser();
+	}
+
+	/**
+	 * Returns the Administrator status of the user.
+	 * 
+	 * @return <code>true</code> if the user is an administrator,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean isAdministrator() {
+		if (userService.isUserAdmin()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns the Coordinator status of the user.
+	 * 
+	 * @return <code>true</code> if the user is an coordinator,
+	 *         <code>false</code> otherwise.
+	 */
+	//TODO: This seems very inefficient. Is there a better way? -damith
+	public boolean isCoordinator() {
+		// Get list of coordinators
+		List<Coordinator> coordinatorList = getCoordinatorList();
+
+		User user = userService.getCurrentUser();
+
+		if (user == null)
+			return false;
+
+		// Check if user is in the list of coordinators
+		for (Coordinator c : coordinatorList) {
+			if (user.getNickname().equalsIgnoreCase(c.getGoogleID())) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+
+	public void deleteCoord(String coordId){
+		Coordinator coord = getCoordinator(coordId);
+		if (coord == null) {
+			String errorMessage = "Trying to delete non-existent coordinator: "
+					+ coordId;
+			log.warning(errorMessage);
+		}
+		getPM().deletePersistent(coord);
+	}
+	public void verifyStudentExists(String courseId, String studentEmail) throws EntityDoesNotExistException {
+		if(getStudent(courseId, studentEmail)==null){
+			throw new EntityDoesNotExistException("The student "+studentEmail+ " does not exist in course "+courseId);
+		}
+		
+	}
+
+}
---
+++ src/main/webapp/jsp/coordEvalSubmissionView.jsp
@@ -0,0 +1,110 @@
+<%@ page import="teammates.common.Common"%>
+<%@ page import="teammates.common.datatransfer.CourseData"%>
+<%@ page import="teammates.common.datatransfer.EvaluationData"%>
+<%@ page import="teammates.common.datatransfer.SubmissionData"%>
+<%@ page import="teammates.ui.controller.CoordEvalSubmissionViewHelper"%>
+<%	CoordEvalSubmissionViewHelper helper = (CoordEvalSubmissionViewHelper)request.getAttribute("helper"); %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<link rel="shortcut icon" href="/favicon.png" />
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
+	<title>Teammates - Coordinator</title>
+	<link rel=stylesheet href="/stylesheets/main.css" type="text/css" />
+	<link rel=stylesheet href="/stylesheets/evaluation.css" type="text/css" />
+	
+	<script language="JavaScript" src="/js/jquery-1.6.2.min.js"></script>
+	<script language="JavaScript" src="/js/tooltip.js"></script>
+	<script language="JavaScript" src="/js/date.js"></script>
+	<script language="JavaScript" src="/js/CalendarPopup.js"></script>
+	<script language="JavaScript" src="/js/AnchorPosition.js"></script>
+	<script language="JavaScript" src="/js/helper.js"></script>
+	<script language="JavaScript" src="/js/common.js"></script>
+	
+	<script language="JavaScript" src="/js/coordinator.js"></script>
+	<script language="JavaScript" src="/js/coordEval.js"></script>
+
+</head>
+
+<body>
+	<div id="dhtmltooltip"></div>
+	<div id="frameTop">
+		<jsp:include page="<%= Common.JSP_COORD_HEADER %>" />
+	</div>
+
+	<div id="frameBody">
+		<div id="frameBodyWrapper">
+			<div id="topOfPage"></div>
+			<div id="headerOperation">
+				<h1>View Student's Evaluation</h1>
+				<table class="evaluation_info">
+					<tr>
+						<td>Course ID:</td>
+						<td><%= helper.evaluation.course %></td>
+					</tr>
+					<tr>
+						<td>Evaluation Name:</td>
+						<td><%=CoordEvalSubmissionViewHelper.escapeForHTML(helper.evaluation.name)%></td>
+					</tr>
+				</table>
+			</div>
+			<div id="studentEvaluationSubmissions">
+			<%
+				for(boolean byReviewee = true, repeat=true; repeat; repeat = byReviewee, byReviewee=false){
+			%>
+				<h2 style="text-align:center"><%=CoordEvalSubmissionViewHelper.escapeForHTML(helper.student.name) + (byReviewee ? "'s Result" : "'s Submission")%></h2>
+				<table class="result_table">
+					<thead><tr>
+						<th colspan="2" width="10%">
+							<span class="fontcolor"><%=byReviewee ? "Reviewee" : "Reviewer"%>: </span><%=helper.student.name%></th>
+						<th><span class="fontcolor"
+								onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_CLAIMED%>')"
+								onmouseout="hideddrivetip()">
+							Claimed Contributions: </span><%=CoordEvalSubmissionViewHelper.printSharePoints(helper.result.claimedToCoord,true)%></th>
+						<th><span class="fontcolor"
+								onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_PERCEIVED%>')"
+								onmouseout="hideddrivetip()">
+							Perceived Contributions: </span><%=CoordEvalSubmissionViewHelper.printSharePoints(helper.result.perceivedToCoord,true)%></th>
+					</tr></thead>
+					<tr>
+						<td colspan="4"><b>Self evaluation:</b><br />
+								<%=CoordEvalSubmissionViewHelper.printJustification(helper.result.getSelfEvaluation())%></td>
+						</tr>
+						<tr>
+							<td colspan="4"><b>Comments about team:</b><br />
+								<%=CoordEvalSubmissionViewHelper.printComments(helper.result.getSelfEvaluation(), helper.evaluation.p2pEnabled)%></td>
+						</tr>
+					<tr class="result_subheader">
+						<td width="15%"><%=byReviewee ? "From" : "To"%> Student</td>
+						<td width="5%">Contribution</td>
+						<td width="40%">Comments</td>
+						<td width="40%">Messages</td>
+					</tr>
+					<%
+						for(SubmissionData sub: (byReviewee ? helper.result.incoming : helper.result.outgoing)){ if(sub.reviewer.equals(sub.reviewee)) continue;
+					%>
+						<tr>
+							<td><b><%=CoordEvalSubmissionViewHelper.escapeForHTML(byReviewee ? sub.reviewerName : sub.revieweeName)%></b></td>
+							<td><%= CoordEvalSubmissionViewHelper.printSharePoints(sub.normalizedToCoord,false) %></td>
+							<td><%= CoordEvalSubmissionViewHelper.printJustification(sub) %></td>
+							<td><%= CoordEvalSubmissionViewHelper.printComments(sub, helper.evaluation.p2pEnabled) %></td>
+						</tr>
+					<%	} %>
+				</table>
+				<br /><br />
+				<% } %>
+				<input type="button" class="button" id="button_back" value="Close"
+						onclick="window.close()"/>
+				<input type="button" class="button" id="button_edit" value="Edit Submission"
+						onclick="window.location.href='<%= helper.getCoordEvaluationSubmissionEditLink(helper.evaluation.course, helper.evaluation.name, helper.student.email) %>'"/>
+				<br /><br />
+			</div>
+		</div>
+	</div>
+
+	<div id="frameBottom">
+		<jsp:include page="<%= Common.JSP_FOOTER %>" />
+	</div>
+</body>
+</html>
