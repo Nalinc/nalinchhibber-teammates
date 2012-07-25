4003f057733a5eea0a05df3b9829ad4cca725f14
aldrianobaja.m <aldrianobaja.m@localhost>
2012-07-25 14:29:22 +0800

    Moved the sources and JavaDocs into /test/resources/lib folder

---
+++ src/main/webapp/js/coordEval.js
@@ -0,0 +1,249 @@
+function isEvaluationNameLengthValid(name) {
+	return name.length <= 22;
+}
+
+function isEvaluationNameValid(name) {
+	if (name.indexOf("\\") >= 0 || name.indexOf("'") >= 0
+			|| name.indexOf("\"") >= 0) {
+		return false;
+	}
+	if (name.match(/^[a-zA-Z0-9 ]*$/) == null) {
+		return false;
+	}
+	return true;
+}
+
+function convertDateFromDDMMYYYYToMMDDYYYY(dateString) {
+	return dateString.substring(3, 5) + "/" +
+			dateString.substring(0, 2) + "/" +
+			dateString.substring(6, 10);
+}
+
+function isAddEvaluationScheduleValid(start, startTime, deadline, deadlineTime) {
+	start = convertDateFromDDMMYYYYToMMDDYYYY(start);
+	deadline = convertDateFromDDMMYYYYToMMDDYYYY(deadline);
+
+	var now = new Date();
+
+	start = new Date(start);
+	deadline = new Date(deadline);
+
+	// If the hour value is 24, then set time to 23:59
+	if (startTime != 24) {
+		start.setHours(startTime);
+	} else {
+		start.setHours(23);
+		start.setMinutes(59);
+	}
+	if (deadlineTime != 24) {
+		deadline.setHours(deadlineTime);
+	} else {
+		deadline.setHours(23);
+		deadline.setMinutes(59);
+	}
+
+	if (start > deadline) {
+		return false;
+	} else if (now > start) {
+		return false;
+	} else if (!(start > deadline || deadline > start)) {
+		if (startTime >= deadlineTime) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function isEditEvaluationScheduleValid(start, startTime, deadline,
+		deadlineTime, timeZone, activated, status) {
+	start = convertDateFromDDMMYYYYToMMDDYYYY(start);
+	deadline = convertDateFromDDMMYYYYToMMDDYYYY(deadline);
+
+	var now = new Date();
+
+	start = new Date(start);
+	deadline = new Date(deadline);
+
+	// If the hour value is 24, then set time to 23:59
+	if (startTime != 24) {
+		start.setHours(startTime);
+	} else {
+		start.setHours(23);
+		start.setMinutes(59);
+	}
+	if (deadlineTime != 24) {
+		deadline.setHours(deadlineTime);
+	} else {
+		deadline.setHours(23);
+		deadline.setMinutes(59);
+	}
+
+	if (start > deadline) {
+		return false;
+	} else if (status == "AWAITING") {
+		// Open evaluation should be done by system only.
+		// Thus, coordinator cannot change evaluation ststus from AWAITING to
+		// OPEN
+		if (start < now) {
+			return false;
+		}
+	}/*else if(!activated && start < now) {
+		return false;
+	}*/
+	
+	return true;
+}
+
+/**
+ * Check whether the evaluation input (which is passed as a form) is valid
+ * @param form
+ * @returns {Boolean}
+ */
+function checkAddEvaluation(form){
+	var courseID = form.courseid.value;
+	var name = form.evaluationname.value;
+	var commentsEnabled = form.commentsstatus.value;
+	var start = form.start.value;
+	var startTime = form.starttime.value;
+	var deadline = form.deadline.value;
+	var deadlineTime = form.deadlinetime.value;
+	var timeZone = form.timezone.value;
+	var gracePeriod = form.graceperiod.value;
+	var instructions = form.instr.value;
+
+	if (courseID == "" || name == "" || start == "" || startTime == ""
+		|| deadline == "" || deadlineTime == "" || timeZone == ""
+			|| gracePeriod == "" || instructions == "") {
+		setStatusMessage(DISPLAY_FIELDS_EMPTY, true);
+		return false;
+	} else if (!isEvaluationNameValid(name)) {
+		setStatusMessage(DISPLAY_EVALUATION_NAMEINVALID, true);
+		return false;
+	} else if (!isEvaluationNameLengthValid(name)) {
+		setStatusMessage(DISPLAY_EVALUATION_NAME_LENGTHINVALID, true);
+		return false;
+	} else if (!isAddEvaluationScheduleValid(start, startTime, deadline, deadlineTime)) {
+		setStatusMessage(DISPLAY_EVALUATION_SCHEDULEINVALID, true);
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Check whether the evaluation input (which is passed as a form) is valid
+ * @param form
+ * @returns {Boolean}
+ */
+function checkEditEvaluation(form){
+	var courseID = form.courseid.value;
+	var name = form.evaluationname.value;
+	var commentsEnabled = form.commentsstatus.value;
+	var start = form.start.value;
+	var startTime = form.starttime.value;
+	var deadline = form.deadline.value;
+	var deadlineTime = form.deadlinetime.value;
+	var timeZone = form.timezone.value;
+	var gracePeriod = form.graceperiod.value;
+	var instructions = form.instr.value;
+
+	if (courseID == "" || name == "" || start == "" || startTime == ""
+		|| deadline == "" || deadlineTime == "" || timeZone == ""
+			|| gracePeriod == "" || instructions == "") {
+		setStatusMessage(DISPLAY_FIELDS_EMPTY, true);
+		return false;
+	} else if (!isEvaluationNameValid(name)) {
+		setStatusMessage(DISPLAY_EVALUATION_NAMEINVALID, true);
+		return false;
+	} else if (!isEvaluationNameLengthValid(name)) {
+		setStatusMessage(DISPLAY_EVALUATION_NAME_LENGTHINVALID, true);
+		return false;
+	} else if (!isEditEvaluationScheduleValid(start, startTime, deadline, deadlineTime)) {
+		setStatusMessage(DISPLAY_EVALUATION_SCHEDULEINVALID, true);
+		return false;
+	}
+	return true;
+}
+
+/**
+ * To be run on page finish loading, this will select the input: start date,
+ * start time, and timezone based on client's time.
+ */
+function selectDefaultTimeOptions(){
+	var now = new Date();
+	
+	var currentDate = convertDateToDDMMYYYY(now);
+	var hours = convertDateToHHMM(now).substring(0, 2);
+	var currentTime = (parseInt(hours) + 1) % 24;
+	var timeZone = -now.getTimezoneOffset() / 60;
+
+	document.getElementById(EVALUATION_START).value = currentDate;
+	document.getElementById(EVALUATION_STARTTIME).value = currentTime;
+	document.getElementById(EVALUATION_TIMEZONE).value = ""+timeZone;
+}
+
+/**
+ * Pops up confirmation dialog whether to delete specified evaluation
+ * @param courseID
+ * @param name
+ * @returns
+ */
+function toggleDeleteEvaluationConfirmation(courseID, name) {
+	return confirm("Are you sure you want to delete the evaluation " + name + " in " + courseID + "?");
+}
+
+/**
+ * Shows the desired evaluation report based on the id.
+ * This is for the evaluation results page.
+ * @param id
+ * 		One of:
+ * 		<ul>
+ * 		<li>coordinatorEvaluationSummaryTable</li>
+ * 		<li>coordinatorEvaluationDetailedReviewerTable</li>
+ * 		<li>coordinatorEvaluationDetailedRevieweeTable</li>
+ * 		</ul>
+ */
+function showReport(id){
+	$(".evaluation_result").hide();
+	$("#"+id).show();
+}
+
+/**
+ * Pops up confirmation dialog whether to publish the specified
+ * evaluation
+ * @param name 
+ */
+function togglePublishEvaluation(name) {
+	return confirm("Are you sure you want to publish the evaluation " + name + "?");
+}
+
+/**
+ * Pops up confirmation dialog whether to unpublish the specified
+ * evaluation
+ * @param name 
+ */
+function toggleUnpublishEvaluation(name){
+	return confirm("Are you sure you want to unpublish the evaluation " + name + "?");
+}
+
+/**
+ * Pops up confirmation dialog whether to remind students to fill in a specified
+ * evaluation.
+ * @param courseID
+ * @param evaluationName
+ */
+function toggleRemindStudents(evaluationName) {
+	return confirm("Send e-mails to remind students who have not submitted their evaluations for " + evaluationName + "?");
+}
+
+/**
+ * Opens new window from link.
+ * Javascript is used here so that the newly opened window will be able to
+ * access this parent window.
+ * @param obj
+ * @returns {Boolean}
+ */
+function openChildWindow(link){
+	window.open(link, "childWindow");
+	return false;
+}
---
+++ src/main/webapp/jsp/coordHome.jsp
@@ -0,0 +1,135 @@
+<%@ page import="teammates.common.Common" %>
+<%@ page import="teammates.common.datatransfer.CourseData"%>
+<%@ page import="teammates.common.datatransfer.EvaluationData"%>
+<%@ page import="teammates.ui.controller.CoordHomeHelper"%>
+<%	CoordHomeHelper helper = (CoordHomeHelper)request.getAttribute("helper"); %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<link rel="shortcut icon" href="/favicon.png" />
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
+	<title>Teammates - Coordinator</title>
+	<link rel=stylesheet href="/stylesheets/main.css" type="text/css" />
+	<link rel=stylesheet href="/stylesheets/evaluation.css" type="text/css" />
+	
+	<script language="JavaScript" src="/js/jquery-1.6.2.min.js"></script>
+	<script language="JavaScript" src="/js/tooltip.js"></script>
+	<script language="JavaScript" src="/js/date.js"></script>
+	<script language="JavaScript" src="/js/CalendarPopup.js"></script>
+	<script language="JavaScript" src="/js/AnchorPosition.js"></script>
+	<script language="JavaScript" src="/js/helper.js"></script>
+	<script language="JavaScript" src="/js/constants.js"></script>
+	<script language="JavaScript" src="/js/common.js"></script>
+	
+	<script language="JavaScript" src="/js/coordinator.js"></script>
+	<script language="JavaScript" src="/js/coordCourse.js"></script>
+	<script language="JavaScript" src="/js/coordEval.js"></script>
+
+</head>
+
+<body>
+	<div id="dhtmltooltip"></div>
+	<div id="frameTop">
+		<jsp:include page="<%= Common.JSP_COORD_HEADER %>" />
+	</div>
+
+	<div id="frameBody">
+		<div id="frameBodyWrapper">
+			<div id="topOfPage"></div>
+			<div id="headerOperation">
+				<h1>Coordinator Home</h1>
+				<br />
+				<div class="result_team">
+					<div class="result_addOrJoinCourse">
+						<a href="<%= helper.getCoordCourseLink() %>" name="addNewCourse" id="addNewCourse">
+							Add New Course </a>
+					</div>
+				</div>
+			</div>
+			<jsp:include page="<%= Common.JSP_STATUS_MESSAGE %>" />
+			<div id="coordinatorHomeTable">
+				<%	int idx = -1;
+					int evalIdx = -1;
+					for (CourseData course: helper.courses) { idx++;
+				%>
+				<div class="result_team home_courses_div" id="course<%= idx %>">
+					<div class="result_homeTitle">
+						<h2>[<%= course.id %>] :
+							<%=CoordHomeHelper.escapeForHTML(course.name)%>
+						</h2>
+					</div>
+					<div class="result_homeLinks">
+						<a class="t_course_enroll<%=idx%>"
+							href="<%=helper.getCoordCourseEnrollLink(course.id)%>"
+							onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_COURSE_ENROLL%>')"
+							onmouseout="hideddrivetip()">
+							Enroll
+						</a>
+						<a class="t_course_view<%=idx%>"
+							href="<%=helper.getCoordCourseDetailsLink(course.id)%>"
+							onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_COURSE_DETAILS%>')"
+							onmouseout="hideddrivetip()">
+							View
+						</a>
+						<a class="t_course_add_eval<%=idx%>"
+							href="<%=helper.getCoordEvaluationLink()%>"
+							onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_COURSE_ADD_EVALUATION%>')"
+							onmouseout="hideddrivetip()">
+							Add Evaluation
+						</a>
+						<a class="t_course_delete<%=idx%>"
+							href="<%=helper.getCoordCourseDeleteLink(course.id,true)%>"
+							onclick="hideddrivetip(); return toggleDeleteCourseConfirmation('<%=course.id%>')"
+							onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_COURSE_DELETE%>')"
+							onmouseout="hideddrivetip()">
+							Delete
+						</a>
+					</div>
+					<div style="clear: both;"></div>
+					<br />
+					<%
+						if (course.evaluations.size() > 0) {
+					%>
+						<table id="dataform">
+							<tr>
+								<th class="leftalign">Evaluation Name</th>
+								<th class="centeralign">Status</th>
+								<th class="centeralign"><span
+									onmouseover="ddrivetip('<%=Common.HOVER_MESSAGE_EVALUATION_RESPONSE_RATE%>')"
+									onmouseout="hideddrivetip()">Response Rate</span></th>
+								<th class="centeralign">Action(s)</th>
+							</tr>
+							<%
+								for (EvaluationData eval: course.evaluations){ evalIdx++;
+							%>
+								<tr class="home_evaluations_row" id="evaluation<%=evalIdx%>">
+									<td class="t_eval_name<%=idx%>"><%=CoordHomeHelper.escapeForHTML(eval.name)%></td>
+									<td class="t_eval_status<%= idx %> centeralign"><span
+										onmouseover="ddrivetip('<%= CoordHomeHelper.getCoordHoverMessageForEval(eval) %>')"
+										onmouseout="hideddrivetip()"><%= CoordHomeHelper.getCoordStatusForEval(eval) %></span></td>
+									<td class="t_eval_response<%= idx %> centeralign"><%= eval.submittedTotal %>
+										/ <%= eval.expectedTotal %></td>
+									<td class="centeralign"><%= helper.getCoordEvaluationActions(eval,evalIdx, true) %>
+									</td>
+								</tr>
+							<%	} %>
+						</table>
+						<br />
+					<%
+						}
+					%>
+				</div>
+				<br /> <br /> <br />
+				<%		out.flush();
+					}
+				%>
+			</div>
+		</div>
+	</div>
+
+	<div id="frameBottom">
+		<jsp:include page="<%= Common.JSP_FOOTER %>" />
+	</div>
+</body>
+</html>
