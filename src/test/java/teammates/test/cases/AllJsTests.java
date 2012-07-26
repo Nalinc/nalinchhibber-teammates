102f342081766b2bfe0a9a61cec1898cfc8d1077
GCT <GCT@GCT-PC>
2012-07-26 15:17:25 +0800

    Issue 244 resolved. Remove redundant comments from the javascript test files

---
+++ src/test/java/teammates/test/cases/AllJsTests.java
@@ -0,0 +1,52 @@
+package teammates.test.cases;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import teammates.test.driver.BrowserInstance;
+import teammates.test.driver.BrowserInstancePool;
+
+public class AllJsTests extends BaseTestCase{
+	
+	//private static WebDriver driver;
+	private static BrowserInstance bi;
+	
+	@BeforeClass
+	public static void setUp() {
+		printTestClassHeader();
+		bi = BrowserInstancePool.getBrowserInstance();
+	}
+
+	@Test
+	public void executeJsTests() throws IOException {
+		 
+		int totalCases = 112; 
+		
+		int totalCasesExpectedToPass = totalCases;
+		
+		print("Going to execute "+totalCasesExpectedToPass+" JavaScript Unit tests...");
+		
+		String workingDirectory = new File(".").getCanonicalPath();
+		bi.goToUrl("file:///"+workingDirectory+"/src/test/javascript/AllJsUnitTests.html");
+
+		bi.waitForElementPresent(By.id("qunit-testresult"));
+		String expectedResultString = totalCasesExpectedToPass+" tests of "+totalCases+" passed";
+		assertTrue(bi.getElementText(By.id("qunit-testresult")).contains(expectedResultString));
+		
+		print("As expected, "+expectedResultString);
+
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		BrowserInstancePool.release(bi);
+		printTestClassFooter();
+	}
+}
