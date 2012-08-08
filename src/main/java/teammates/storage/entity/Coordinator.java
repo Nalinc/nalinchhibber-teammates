69d6ece442518b2bcca350d69107188ea480c76c
Goh Chun Teck <chunteck.88@gmail.com>
2012-08-08 17:48:50 +0800

    Improve the quality of JS tests. Still waiting for CoordCourseEnrollJsTest.js from Issue 261 as well Update Issue 259

---
+++ src/main/java/teammates/storage/entity/Coordinator.java
@@ -0,0 +1,17 @@
+package teammates.storage.entity;
+
+import javax.jdo.annotations.PersistenceCapable;
+
+
+/**
+ * Coordinator is a persistent data class that holds information pertaining to a
+ * coordinator on Teammates. 
+ */
+@PersistenceCapable
+public class Coordinator extends Account {
+	
+	public Coordinator(String googleID, String name, String email) {
+		super(googleID, name, email);
+	}
+	
+}
