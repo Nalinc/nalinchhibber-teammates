a4468aa2139f547203ac5356dea8a0e3c72061d3
Goh Chun Teck <chunteck.88@gmail.com>
2012-08-14 19:50:22 +0800

    Fixed the heading as well as the replacement of p2pfeedback with justification comment Update Issue 270

---
+++ src/test/javascript/CommonJsTest.js
@@ -0,0 +1,35 @@
+
+module('common.js');
+
+
+test('scrollToTop()', function(){
+	// N/A, trivial function
+	expect(0);
+});
+
+
+test('setStatusMessage(message,error)', function(){	
+	$("body").append('<div id="statusMessage"></div>');
+	var message = "Status Message";
+	
+	setStatusMessage(message, false);
+	equal($("#statusMessage").html(), message, "Normal status message");
+	ok(($("#statusMessage").css('background-color') == "rgba(0, 0, 0, 0)" || $("#statusMessage").css('background-color') == "transparent"), "No background");
+	setStatusMessage("", false);
+	equal($("#statusMessage").html(), "", "Empty status message");
+	ok(($("#statusMessage").css('background-color') == "rgba(0, 0, 0, 0)" || $("#statusMessage").css('background-color') == "transparent"), "No background");
+	setStatusMessage(message, true);
+	equal($("#statusMessage").html(), message, "Normal status message");
+	equal($("#statusMessage").css('background-color'), "rgb(255, 153, 153)", "Red background");
+	setStatusMessage("", true);
+	equal($("#statusMessage").html(), "", "Normal status message");
+	ok(($("#statusMessage").css('background-color') == "rgba(0, 0, 0, 0)" || $("#statusMessage").css('background-color') == "transparent"), "No background");
+	
+});
+
+
+test('clearStatusMessage()', function(){
+	clearStatusMessage();
+	equal($("#statusMessage").html(), "", "Status message cleared");
+	ok(($("#statusMessage").css('background-color') == "rgba(0, 0, 0, 0)" || $("#statusMessage").css('background-color') == "transparent"), "No background");
+});
---
+++ src/test/javascript/CoordEvalResultsJsTest.js
@@ -0,0 +1,14 @@
+
+module('coordEvalResults.js');
+
+
+test('showReport(id)', function(){
+	// N/A, uses elements in the page
+	expect(0);
+});
+
+
+test('openChildWindow(link)', function(){
+	// N/A, opens new window
+	expect(0);
+});
