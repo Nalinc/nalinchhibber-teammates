c3ecb5898a5bd89eb9d9d0bebc97f44b8a0a1828
Goh Chun Teck <chunteck.88@gmail.com>
2012-09-04 14:04:35 +0800

    starting Issue 293[Create HTML style guide] Update Issue 293

---
+++ src/main/java/teammates/logic/automated/EvaluationClosingRemindersServlet.java
@@ -0,0 +1,24 @@
+package teammates.logic.automated;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import teammates.logic.backdoor.BackDoorLogic;
+
+@SuppressWarnings("serial")
+public class EvaluationClosingRemindersServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			new BackDoorLogic().sendRemindersForClosingEvaluations();
+		} catch (Exception e) {
+			throw new RuntimeException(
+					"Unexpected exception while sending reminders for closing evaluations"
+							+ e);
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		doGet(req, resp);
+	}
+}
