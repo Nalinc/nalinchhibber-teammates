79a0981b337bc2f6619f0a927995a1ef91ffe4bb
Kenny <whisperingrwind@gmail.com>
2012-10-06 17:40:08 +0800

    Start Issue 298 verified logical components test cases coverage

---
+++ src/main/java/teammates/common/datatransfer/CourseData.java
@@ -0,0 +1,100 @@
+package teammates.common.datatransfer;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import teammates.common.Common;
+import teammates.storage.entity.Course;
+
+public class CourseData {
+	public String id;
+	public String name;
+	public String coord;
+
+	// these are marked transient because we don't want to involve them in
+	// Json conversions.
+	public transient int teamsTotal = Common.UNINITIALIZED_INT;
+	public transient int studentsTotal = Common.UNINITIALIZED_INT;
+	public transient int unregisteredTotal = Common.UNINITIALIZED_INT;
+	public transient ArrayList<EvaluationData> evaluations = new ArrayList<EvaluationData>();
+	public transient ArrayList<TeamData> teams = new ArrayList<TeamData>();
+	// TODO: to be removed as we don't allow loners
+	public transient ArrayList<StudentData> loners = new ArrayList<StudentData>();
+
+	public static final String ERROR_FIELD_ID = "Course ID cannot be null or empty\n";
+	public static final String ERROR_ID_TOOLONG = "Course ID cannot be more than "
+			+ Common.COURSE_ID_MAX_LENGTH + " characters\n";
+	public static final String ERROR_ID_INVALIDCHARS = "Course ID can have only alphabets, numbers, dashes, underscores, and dollar sign\n";
+	public static final String ERROR_FIELD_NAME = "Course name cannot be null or empty\n";
+	public static final String ERROR_NAME_TOOLONG = "Course name cannot be more than "
+			+ Common.COURSE_NAME_MAX_LENGTH + " characters\n";
+	public static final String ERROR_FIELD_COORD = "Course must belong to a Coordinator\n";
+
+	private static Logger log = Common.getLogger();
+
+	public CourseData() {
+
+	}
+
+	public CourseData(String id, String name, String coordId) {
+		this.id = id;
+		this.name = name;
+		this.coord = coordId;
+	}
+
+	public CourseData(Course course) {
+		this.id = course.getID();
+		this.name = course.getName();
+		this.coord = course.getCoordinatorID();
+	}
+
+	public Course toEntity() {
+		return new Course(id, name, coord);
+	}
+
+	public boolean isValid() {
+
+		if (this.id == null || this.id == ""
+				|| this.id.length() > Common.COURSE_ID_MAX_LENGTH
+				|| this.name == null || this.name == ""
+				|| this.name.length() > Common.COURSE_NAME_MAX_LENGTH
+				|| !this.id.matches("^[a-zA-Z_$0-9.-]+$") || this.coord == null
+				|| this.coord == "") {
+			return false;
+		}
+
+		return true;
+	}
+
+	public String getInvalidStateInfo() {
+		String errorMessage = "";
+
+		// Validate ID not null, empty, less than max length and acceptable
+		// format
+		if (this.id == null || this.id == "") {
+			errorMessage += ERROR_FIELD_ID;
+		} else {
+
+			if (this.id.length() > Common.COURSE_ID_MAX_LENGTH) {
+				errorMessage += ERROR_ID_TOOLONG;
+			}
+
+			if (!this.id.matches("^[a-zA-Z_$0-9.-]+$")) {
+				errorMessage += ERROR_ID_INVALIDCHARS;
+			}
+		}
+
+		// Validate name not null, empty and less than max length
+		if (this.name == null || this.name == "") {
+			errorMessage += ERROR_FIELD_NAME;
+		} else if (name.length() > Common.COURSE_NAME_MAX_LENGTH) {
+			errorMessage += ERROR_NAME_TOOLONG;
+		}
+
+		if (this.coord == null || this.coord == "") {
+			errorMessage += ERROR_FIELD_COORD;
+		}
+
+		return errorMessage;
+	}
+}
---
+++ src/main/java/teammates/common/datatransfer/SubmissionData.java
@@ -0,0 +1,161 @@
+package teammates.common.datatransfer;
+
+import static teammates.common.Common.EOL;
+
+import java.util.logging.Logger;
+
+import teammates.common.Common;
+import teammates.storage.entity.Submission;
+
+import com.google.appengine.api.datastore.Text;
+
+public class SubmissionData {
+	/** course ID */
+	public String course;
+
+	/** evaluation name */
+	public String evaluation;
+
+	/** team name */
+	public String team;
+
+	/** reviewer email */
+	public String reviewer;
+
+	public transient String reviewerName = null;
+
+	/** reviewee email */
+	public String reviewee;
+
+	public transient String revieweeName = null;
+
+	public int points;
+
+	public Text justification;
+
+	public Text p2pFeedback;
+
+	public transient int normalizedToStudent = Common.UNINITIALIZED_INT;
+
+	public transient int normalizedToCoord = Common.UNINITIALIZED_INT;
+	
+	public static final String ERROR_FIELD_COURSE = "Submission must belong to a course\n";
+	public static final String ERROR_FIELD_EVALUATION = "Submission must belong to an evaluation";
+	public static final String ERROR_FIELD_REVIEWEE = "Submission reviewee cannot be null or empty\n";
+	public static final String ERROR_FIELD_REVIEWER = "Submission reviewer cannot be null or empty\n";
+
+
+	private static Logger log = Common.getLogger();
+	
+	public SubmissionData() {
+
+	}
+
+	public SubmissionData(String courseId, String evalName, String team,
+			String toStudent, String fromStudent) {
+		this.course = courseId;
+		this.evaluation = evalName;
+		this.team = team;
+		this.reviewee = toStudent;
+		this.reviewer = fromStudent;
+	}
+
+	public SubmissionData(Submission s) {
+		this.course = s.getCourseID();
+		this.evaluation = s.getEvaluationName();
+		this.reviewer = s.getFromStudent();
+		this.reviewee = s.getToStudent();
+		this.team = s.getTeamName();
+		this.points = s.getPoints();
+		this.justification = s.getJustification();
+		this.p2pFeedback = s.getCommentsToStudent();
+	}
+
+	public Submission toEntity() {
+		return new Submission(reviewer, reviewee, course, evaluation, team);
+	}
+
+	/**
+	 * using a simple copy method instead of clone(). Reason: seems it is overly
+	 * complicated and not well thought out see
+	 * http://stackoverflow.com/questions
+	 * /2326758/how-to-properly-override-clone-method
+	 * 
+	 * @return a copy of the object
+	 */
+	public SubmissionData getCopy() {
+		SubmissionData copy = new SubmissionData();
+		copy.course = this.course;
+		copy.evaluation = this.evaluation;
+		copy.team = this.team;
+		copy.reviewer = this.reviewer;
+		copy.reviewerName = this.reviewerName;
+		copy.reviewee = this.reviewee;
+		copy.revieweeName = this.revieweeName;
+		copy.points = this.points;
+		copy.justification = new Text(justification == null ? null
+				: justification.getValue());
+		copy.p2pFeedback = new Text(p2pFeedback == null ? null
+				: p2pFeedback.getValue());
+		copy.normalizedToStudent = this.normalizedToStudent;
+		copy.normalizedToCoord = this.normalizedToCoord;
+		return copy;
+	}
+
+	public boolean isSelfEvaluation() {
+		return reviewee.equals(reviewer);
+	}
+
+	public String toString() {
+		return toString(0);
+	}
+
+	public String toString(int indent) {
+		String indentString = Common.getIndent(indent);
+		StringBuilder sb = new StringBuilder();
+		sb.append(indentString + "[eval:" + evaluation + "] " + reviewer + "->"
+				+ reviewee + EOL);
+		sb.append(indentString + " points:" + points);
+		sb.append(" [normalized-to-student:" + normalizedToStudent + "]");
+		sb.append(" [normalized-to-coord:" + normalizedToStudent + "]");
+		sb.append(EOL + indentString + " justificatoin:"
+				+ justification.getValue());
+		sb.append(EOL + indentString + " p2pFeedback:" + p2pFeedback.getValue());
+		return sb.toString();
+	}
+
+	public boolean isValid() {
+
+		if (this.course == null || this.course == "" || this.evaluation == null
+				|| this.evaluation == "" || this.reviewee == null
+				|| this.reviewee == "" || this.reviewer == null
+				|| this.reviewer == "") {
+			return false;
+		}
+
+		return true;
+	}
+
+	public String getInvalidStateInfo() {
+		String errorMessage = "";
+
+		if (this.course == null || this.course == "") {
+			errorMessage += ERROR_FIELD_COURSE;
+		}
+
+		if (this.evaluation == null || this.evaluation == "") {
+			errorMessage += ERROR_FIELD_EVALUATION;
+		}
+
+		if (this.reviewee == null || this.reviewee == "") {
+			errorMessage += ERROR_FIELD_REVIEWEE;
+		}
+		
+		if (this.reviewer == null || this.reviewer == "") {
+			errorMessage += ERROR_FIELD_REVIEWER;
+		}
+
+		return errorMessage;
+	}
+
+}
