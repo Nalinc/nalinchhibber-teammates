9e67389d8b13111aa648399a206b76059285b360
Damith Rajapakse <damith@gmail.com>
2012-12-15 19:21:04 +0800

    Starting Issue 411 [Move 'ideas and wishlist' from Spec to P&S document] Update Issue 411 Moved Ideas and wishlist out of spec.html Removed duplicate spec.html

---
+++ src/main/java/teammates/common/datatransfer/DataBundle.java
@@ -0,0 +1,16 @@
+package teammates.common.datatransfer;
+
+import java.util.HashMap;
+
+/**
+ * This is a class to hold a bundle of entities
+ * This class is mainly used for serializing JSON strings
+ */
+public class DataBundle {
+	
+	public HashMap<String,InstructorData> instructors = new HashMap<String,InstructorData>();
+	public HashMap<String, CourseData> courses = new HashMap<String, CourseData>();
+	public HashMap<String, StudentData> students = new HashMap<String, StudentData>();
+	public HashMap<String, EvaluationData> evaluations = new HashMap<String, EvaluationData>();
+	public HashMap<String, SubmissionData> submissions = new HashMap<String, SubmissionData>();
+}
