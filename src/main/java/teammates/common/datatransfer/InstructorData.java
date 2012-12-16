c798d252477dcdc788d358a4cd0944afab2567a7
Goh Chun Teck <chunteck.88@gmail.com>
2012-12-16 14:54:47 +0800

    Starting Issue 437 [Modify Add Course page to handle multiple instructors] Update Issue 437 Preliminary update to support multiple instructors. UI tests will still fail, require Kenny's functions before I can get them to work add course form modified to include 1 more field show example of instructor list format check instructor list and ensure it is valid new JS tests to for above mentioned check

---
+++ src/main/java/teammates/common/datatransfer/InstructorData.java
@@ -0,0 +1,57 @@
+package teammates.common.datatransfer;
+
+import java.util.HashMap;
+
+import teammates.common.Common;
+import teammates.storage.entity.Coordinator;
+
+public class InstructorData extends BaseData {
+	public HashMap<String, CourseData> courses;
+	public String id;
+	public String name;
+	public String email;
+	
+	public static final String ERROR_FIELD_ID = "Instructor ID is invalid\n";
+	public static final String ERROR_FIELD_NAME = "Instructor name cannot be null or empty\n";
+	public static final String ERROR_FIELD_EMAIL = "Instructor email is invalid\n";
+	
+	public InstructorData(String id, String name, String email) {
+		this();
+		this.id = trimIfNotNull(id);
+		this.name = trimIfNotNull(name);
+		this.email = trimIfNotNull(email);
+	}
+
+	public InstructorData(Coordinator instructor) {
+		this();
+		this.id = instructor.getGoogleID();
+		this.name = instructor.getName();
+		this.email = instructor.getEmail();
+	}
+
+	public InstructorData() {
+		
+	}
+
+	public Coordinator toEntity() {
+		return new Coordinator(id, name, email);
+	}
+
+	public String getInvalidStateInfo() {
+		String errorMessage = "";
+
+		if (!Common.isValidGoogleId(id)) {
+			errorMessage += ERROR_FIELD_ID;
+		}
+
+		if (!Common.isValidName(name)) {
+			errorMessage += ERROR_FIELD_NAME;
+		}
+
+		if (!Common.isValidEmail(email)) {
+			errorMessage += ERROR_FIELD_EMAIL;
+		}
+
+		return errorMessage;
+	}
+}
