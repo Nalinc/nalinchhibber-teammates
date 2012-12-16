4a791e422f819a3bac8d98e4fda301452802f00e
Goh Chun Teck <chunteck.88@gmail.com>
2012-12-16 10:16:15 +0800

    Handling Issue 310[Allow apostrophe in student names] Update Issue 310 Refactored the isStudentNameValid function according to changes requested

---
+++ src/test/java/teammates/test/cases/AllJsTests.java
@@ -0,0 +1,56 @@
+package teammates.test.cases;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import teammates.test.driver.BrowserInstance;
+import teammates.test.driver.BrowserInstancePool;
+
+public class AllJsTests extends BaseTestCase{
+	
+	//private static WebDriver driver;
+	private static BrowserInstance bi;
+	
+	@BeforeClass
+	public static void setUp() {
+		printTestClassHeader();
+		bi = BrowserInstancePool.getBrowserInstance();
+	}
+
+	@Test
+	public void executeJsTests() throws IOException {
+		/*
+		 * Total cases has to be updated based on the number of new javascript
+		 * tests added. This total case number should reflect the number of tests
+		 * in AllJsUnitTests.html.
+		 */
+		int totalCases = 113; 
+		
+		int totalCasesExpectedToPass = totalCases;
+		
+		print("Going to execute "+totalCasesExpectedToPass+" JavaScript Unit tests...");
+		
+		String workingDirectory = new File(".").getCanonicalPath();
+		bi.goToUrl("file:///"+workingDirectory+"/src/test/javascript/AllJsUnitTests.html");
+
+		bi.waitForElementPresent(By.id("qunit-testresult"));
+		String expectedResultString = totalCasesExpectedToPass+" tests of "+totalCases+" passed";
+		assertTrue(bi.getElementText(By.id("qunit-testresult")).contains(expectedResultString));
+		
+		print("As expected, "+expectedResultString);
+
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		BrowserInstancePool.release(bi);
+		printTestClassFooter();
+	}
+}
