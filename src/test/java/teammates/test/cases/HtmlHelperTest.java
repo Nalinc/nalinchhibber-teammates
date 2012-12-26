6ce148f9f1f4e064c041b9fd8b0fd389d57c7821
Goh Chun Teck <chunteck.88@gmail.com>
2012-12-26 14:59:19 +0800

    Starting Issue 449[Block students editing overdue submissions] Update Issue 449 Instead of going into the submission page, student will be redirected to the home page with a status message

---
+++ src/test/java/teammates/test/cases/HtmlHelperTest.java
@@ -0,0 +1,44 @@
+package teammates.test.cases;
+
+import static org.junit.Assert.assertEquals;
+import static teammates.common.Common.EOL;
+
+import java.io.IOException;
+
+import javax.xml.transform.TransformerException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import teammates.test.driver.HtmlHelper;
+
+public class HtmlHelperTest {
+	
+	@Test
+	public void testComparison() throws SAXException, IOException, TransformerException{
+		String expected = "<html></html>";
+		String actual = expected;
+		HtmlHelper.assertSameHtml(expected, actual);
+		
+		actual = "<html> </html>";
+		HtmlHelper.assertSameHtml(expected, actual);
+		
+		expected = "<HTML><HEAD><SCRIPT language=\"JavaScript\" src=\"a.js\" ></SCRIPT></HEAD><BODY id=\"5\"><P>abc</P><DIV id=\"frameBottom\"><DIV></DIV></DIV></BODY></HTML>";
+		actual = expected.replace("<HEAD>", "    <HEAD>    \t"+EOL);
+		HtmlHelper.assertSameHtml(expected, actual);
+		
+		//change attribute order
+		actual = expected.replace("language=\"JavaScript\" src=\"a.js\"", "  src=\"a.js\"   language=\"JavaScript\"  ");
+		HtmlHelper.assertSameHtml(expected, actual);
+		
+		actual = expected.replace("<P>", "<P>\n\n"+EOL+EOL);
+		HtmlHelper.assertSameHtml(expected, actual);
+		
+		actual = expected.replace("<DIV></DIV></DIV>", EOL+EOL+"\n<DIV>\n\n</DIV></DIV>\n\n"+EOL);
+		HtmlHelper.assertSameHtml(expected, actual);
+
+	}
+	
+
+}
