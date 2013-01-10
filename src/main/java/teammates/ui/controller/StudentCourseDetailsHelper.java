c40ee1990f527c490fd9850b0a94c043e366aa09
Damith Rajapakse <damith@gmail.com>
2013-01-10 20:21:14 +0800

    Starting Issue 476 [instructor:help: add a section explaining how to interpret/use evaluation results] Update Issue 476 Removed two unused test pages. Updated 'Tips' section of instructorHelp page with tips on how to interpret results.

---
+++ src/main/java/teammates/ui/controller/StudentCourseDetailsHelper.java
@@ -0,0 +1,15 @@
+package teammates.ui.controller;
+
+import java.util.List;
+
+import teammates.common.datatransfer.CourseData;
+import teammates.common.datatransfer.StudentData;
+import teammates.common.datatransfer.TeamData;
+import teammates.common.datatransfer.InstructorData;
+
+public class StudentCourseDetailsHelper extends Helper {
+	public CourseData course;
+	public StudentData student;
+	public TeamData team;
+	public List<InstructorData> instructors;
+}
