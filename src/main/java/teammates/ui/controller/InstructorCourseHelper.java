71b819bca4135d56754edbe6f63416d0bf91f76f
Damith Rajapakse <damith@gmail.com>
2013-02-28 19:07:46 +0800

    Starting Issue608a [Improve testing for table sorting in the UI] Update Issue 608 Compacted multiple commits into a single commit in new branch

---
+++ src/main/java/teammates/ui/controller/InstructorCourseHelper.java
@@ -0,0 +1,12 @@
+package teammates.ui.controller;
+
+import java.util.List;
+
+import teammates.common.datatransfer.CourseData;
+
+public class InstructorCourseHelper extends Helper{
+	public String courseID;
+	public String courseName;
+	public String instructorList;
+	public List<CourseData> courses;
+}
