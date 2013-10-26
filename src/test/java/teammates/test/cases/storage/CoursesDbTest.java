c3932c8d866643da874f97c4771e37c3aafcc023
Damith Rajapakse <damith@gmail.com>
2013-10-26 14:33:43 +0800

    merging tag V4.70

---
+++ src/test/java/teammates/test/cases/storage/CoursesDbTest.java
@@ -0,0 +1,110 @@
+package teammates.test.cases.storage;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.CourseAttributes;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.common.util.Const;
+import teammates.storage.api.CoursesDb;
+import teammates.test.cases.BaseComponentTestCase;
+import teammates.test.cases.logic.LogicTest;
+
+public class CoursesDbTest extends BaseComponentTestCase {
+
+	private CoursesDb coursesDb = new CoursesDb();
+	
+	@BeforeClass
+	public static void setupClass() throws Exception {
+		printTestClassHeader();
+	}
+
+	@Test
+	public void testCreateCourse() throws EntityAlreadyExistsException, InvalidParametersException {
+		
+		/*Explanation:
+		 * This is an inherited method from EntitiesDb and should be tested in
+		 * EntitiesDbTest class. We test it here too because the method in
+		 * the parent class actually calls an overridden method from the SUT.
+		 */
+
+		______TS("success: typical case");
+		
+		CourseAttributes c = new CourseAttributes();
+		c.id = "CDbT.tCC.newCourse";
+		c.name = "Basic Computing";
+		coursesDb.createEntity(c);
+		LogicTest.verifyPresentInDatastore(c);
+		
+		
+	}
+	
+	@Test
+	public void testGetCourse() throws InvalidParametersException {
+		CourseAttributes c = createNewCourse();
+		
+		// Get existent
+		CourseAttributes retrieved = coursesDb.getCourse(c.id);
+		assertNotNull(retrieved);
+		
+		// Get non-existent - just return null
+		retrieved = coursesDb.getCourse("non-existent-course");
+		assertNull(retrieved);
+		
+		// Null params check:
+		try {
+			coursesDb.getCourse(null);
+			Assert.fail();
+		} catch (AssertionError a) {
+			assertEquals(Const.StatusCodes.DBLEVEL_NULL_INPUT, a.getMessage());
+		}
+	}
+	
+	@Test
+	public void testUpdateCourse() {
+		// Not implemented
+	}
+	
+	@Test
+	public void testDeleteCourse() throws InvalidParametersException {
+		CourseAttributes c = createNewCourse();
+		
+		// Delete
+		coursesDb.deleteCourse(c.id);
+		
+		CourseAttributes deleted = coursesDb.getCourse(c.id);
+		assertNull(deleted);
+		
+		// delete again - should fail silently
+		coursesDb.deleteCourse(c.id);
+		
+		// Null params check:
+		try {
+			coursesDb.deleteCourse(null);
+			Assert.fail();
+		} catch (AssertionError a) {
+			assertEquals(Const.StatusCodes.DBLEVEL_NULL_INPUT, a.getMessage());
+		}
+	}
+
+	private CourseAttributes createNewCourse() throws InvalidParametersException {
+		CourseAttributes c = new CourseAttributes();
+		c.id = "Computing101";
+		c.name = "Basic Computing";
+		
+		try {
+			coursesDb.createEntity(c);
+		} catch (EntityAlreadyExistsException e) {
+			//It is ok if it already exists.
+			ignoreExpectedException();
+		}
+		
+		return c;
+	}
+}
